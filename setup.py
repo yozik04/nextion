26a240aebb6c0d7303b2a1a4d33946a35ae4f2e3
Jevgeni Kiski <yozik04@gmail.com>
2020-04-23 23:45:07 +0300

    Version 1.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename, parent=None):
 
 setup(
     name="nextion",
-    version="1.4.0",
+    version="1.5.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     python_requires=">=3.5.1, <4",
     license="LGPL 3",
