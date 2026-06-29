c44ae40724292a1552d1020a1062f604a67c68a4
Jevgeni Kiski <yozik04@gmail.com>
2026-06-29 10:06:27 +0300

    3.0.0

--- nextion/__init__.py
+++ nextion/__init__.py
@@ -1,7 +1,7 @@
 from nextion.client import EventType, Nextion
 from nextion.exceptions import CommandFailed, CommandTimeout, ConnectionFailed
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 __all__ = [
     "Nextion",
